7cd35faf9fa7593b6843e747da3b3a45439a7ebb
Grant Forrest <gaforres@gmail.com>
2018-02-10 14:05:51 -0500

    add index file

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default } from './withAuthorization';
+export { default as AuthorizationError } from './errors/AuthorizationError';
+import * as r from './resolvers';
+export const resolvers = r;
